2497a4c2eb4afc7c7a66d38b8b33289b5242bbdc
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2019-07-22 16:05:21 +0530

    armstrong no

---
+++ Armstrong.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc  = new Scanner(System.in);
+	    int n = 0,c = 0,sum=0,d=0;
+		if(sc.hasNextInt())
+		    n = sc.nextInt();
+		int no = n;
+		while(no > 0){
+		    d = no%10;
+		    sum+= d*d*d;
+		    no = no/10;
+		}
+		if(sum==n)
+		    System.out.println("Armstrong no");
+		else
+		    System.out.println("Not Armstrong");
+	}
+}
