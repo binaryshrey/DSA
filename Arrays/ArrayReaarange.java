c52580fc7eec696b58908d5be6ac03e725b44d13
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2019-08-22 21:57:03 +0530

    A[i] = i

---
+++ Arrays/ArrayReaarange.java
@@ -0,0 +1,41 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int c=0;
+		int arr[] = new int[n];
+		for(int i=0;i<n;i++){
+		    arr[i] = sc.nextInt();
+		}
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for(int i=0;i<arr.length;i++){
+		    for(int j=0;j<arr.length;j++){
+		        if(i==arr[j]){
+		            list.add(i);
+		            c=1;
+		            break;
+		        }
+		        
+		    }
+		    if(c==0){
+		        list.add(-1);
+		    }
+		    c=0;
+		}
+		
+		for(int i=0;i<list.size();i++){
+		    System.out.print(list.get(i)+" ");
+		}
+		
+	}
+}
