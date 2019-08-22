1f25b735fa29f67a190a602b3d3eb17315e6597c
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2019-08-22 21:55:24 +0530

    Pairwise Sum

---
+++ Arrays/PairSum.java
@@ -0,0 +1,36 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    public static void pairSum(int arr[] ,int d){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]+arr[j]==d){
+                    System.out.print("("+arr[i]+","+arr[j]+")");
+                    System.out.println();
+                }
+            }
+        }
+    }
+    
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int d = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i=0;i<n;i++){
+		    arr[i] = sc.nextInt();
+		}
+		
+		pairSum(arr,d);
+		
+	}
+}
