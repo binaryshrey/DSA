c0abcace0175d0971f355a6764a1ee7e7fe6ff97
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2019-08-23 13:35:06 +0530

    Subarrays for a given array

---
+++ Arrays/Subarrays.java
@@ -0,0 +1,36 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    public static void subArray(int arr[]){
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                for(int k=i;k<=j;k++)
+                    System.out.print(arr[k]+" ");
+            System.out.println();
+                
+            }
+            
+        }
+    }
+    
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i=0;i<n;i++)
+		    arr[i] = sc.nextInt();
+		
+		subArray(arr);
+		
+		
+	}
+}
