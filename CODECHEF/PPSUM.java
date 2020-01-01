3cbca668c8f91218cdc6420d69751cda6164939d
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2020-01-01 21:41:49 +0530

    added CODECHEF soln

---
+++ CODECHEF/PPSUM.java
@@ -0,0 +1,31 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int sum = 0,D=0,N=0,T=0;
+		T = sc.nextInt();
+		if(T>=1 && T<=16){
+		    for(int i=1;i<=T;i++){
+		        D = sc.nextInt();
+		        N = sc.nextInt();
+		        if(D>=1 && N<=4){
+		            for(int j=1;j<=D*N;j++){
+		                sum+=j;
+		            }
+		        }
+		        System.out.println(sum);
+		        sum=0;
+		    }
+		}
+		
+	}
+}
